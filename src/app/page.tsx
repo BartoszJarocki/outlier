007af57fc6eb47af560b413c2393277c8fd24e55
Bartosz Jarocki <bartosz.jarocki@hey.com>
2024-01-16 23:16:16 +0100

    Added FAQ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../components/ui/accordion";
 import { Badge } from "../components/ui/badge";
 import { Button } from "../components/ui/button";
 import {
@@ -8,6 +14,34 @@ import {
   CardContent,
   CardFooter,
 } from "../components/ui/card";
+
+const FAQ_ITEMS = [
+  {
+    title: "How is Outlier different than other agencies?",
+    description:
+      "Outlier operates without signing any contracts. You subscribe, I start work. You can pause or cancel at any time using the customer portal. Also, Outlier is just one person (me), so know who will be working on your project. This also means limited availability.",
+  },
+  {
+    title: "What happens after I subscribe?",
+    description:
+      "I'll contact you to schedule a 30min meeting to discuss your needs. If we're on the same page, we'll set up all the accesses needed and start working. If for some reason there's no fit, you'll get a full refund.",
+  },
+  {
+    title: "How do I request work?",
+    description:
+      "If you're already using some kind of issue tracking software, I'll need an access to it. If you don't I'll set up an account for you where you can request new work and see the progress.",
+  },
+  {
+    title: "How much time do you need to finish the work request?",
+    description:
+      "This really depends on the scope of the work. You should aim to have the work well defined, as it'll decrease back & forth when making sure we're on the same page. Initial work requests can take longer due to the initial setup and getting familiar with the product.",
+  },
+  {
+    title: "What if I only have a single request?",
+    description:
+      "That's ok. You can pause your subscription when the work is done and return when you have additional request.",
+  },
+];
 
 export default function Home() {
   return (
@@ -27,7 +61,7 @@ export default function Home() {
 
           <p className="text-base text-wrap">
             <a href="https://jarocki.me" target="_blank" className="underline">
-              I&apos;m Bartosz, an experienced product engineer based in Poland.
+              I&apos;m Bartosz, an experienced product engineer.
             </a>{" "}
             Outlier is a tiny development studio where I focus on on helping
             startups build and improve their products based on{" "}
@@ -37,11 +71,17 @@ export default function Home() {
             framework.
           </p>
         </section>
+        <section className="flex min-h-0 gap-y-2 justify-start gap-x-4 items-center">
+          <h2 className="text-sm">Current availability</h2>
+          <p>
+            <Badge variant="outline">Limited</Badge>
+          </p>
+        </section>
         <section className="flex flex-col md:flex-row gap-6 md:-m-6 items-stretch">
           <Card className="md:max-w-96 flex-1">
             <CardHeader>
               <div>
-                <Badge>5999€ per month</Badge>
+                <Badge>6000€ per month</Badge>
               </div>
               <CardTitle>One request at a time</CardTitle>
               <CardDescription className="max-w-md">
@@ -60,7 +100,7 @@ export default function Home() {
             </CardContent>
             <CardFooter className="flex flex-col gap-y-2">
               <Button className="w-full" asChild>
-                <a href="https://buy.stripe.com/5kA28TakD19zeVa8wx">
+                <a href="https://buy.stripe.com/4gw3cX64n8C14gw6oq">
                   Subscribe
                 </a>
               </Button>
@@ -72,7 +112,7 @@ export default function Home() {
           <Card className="md:max-w-96 flex-1">
             <CardHeader>
               <div>
-                <Badge>Requires budget of 15 000€+ </Badge>
+                <Badge>Requires budget of 15 000€+</Badge>
               </div>
               <CardTitle>Bring idea to life</CardTitle>
               <CardDescription className="max-w-md">
@@ -97,6 +137,17 @@ export default function Home() {
               </div>
             </CardFooter>
           </Card>
+        </section>
+        <section>
+          <h2 className="text-xl">FAQ</h2>
+          {FAQ_ITEMS.map((item) => (
+            <Accordion type="single" collapsible key={item.title}>
+              <AccordionItem value={item.title}>
+                <AccordionTrigger>{item.title}</AccordionTrigger>
+                <AccordionContent>{item.description}</AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))}
         </section>
       </main>
     </div>
