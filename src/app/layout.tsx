211112b2e871962efa94ba8196fbbd2b8790b0fc
Bartosz Jarocki <bartosz.jarocki@hey.com>
2024-01-18 22:42:06 +0100

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <body className={cn("dark", inter.className)}>{children}</body>
     </html>
   );
